aade1737b4b455f64239471929fbc0f18df48187
Bikal <bikaladhikari5@gmail.com>
2024-04-14 21:08:54 +1000

    importing data and destructuring from data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { FaBeer } from "react-icons/fa";
+import people from "./data";
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people;
   return;
   <div>
     <h2>Reviews Starter</h2>;
